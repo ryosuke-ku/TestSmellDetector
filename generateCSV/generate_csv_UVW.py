0bd3233bd4c1d6d683e1f62836524f2c324b7255
Kurachi <naist1020@gmail.com>
2019-11-03 10:38:24 +0900

    first commit

---
+++ generateCSV/generate_csv_UVW.py
@@ -0,0 +1,50 @@
+from pymongo import MongoClient
+import csv
+
+
+clint = MongoClient()
+db = clint['testMapList']
+
+# items = db.mapingCollection.find({'startline1':11,'endline1':16})
+items = db.mapingCollection.find()
+cnt = 0
+num = 0
+file = open('mongoInfo_UVW.csv','w')
+writer = csv.writer(file)
+
+array = []
+for item in items:
+    project_name = item['path'][:item['path'].find('/')]
+    # print(project_name)
+    init = item['path'][:1].upper()
+    ppath_mongo = item['path'].replace('/','\\')
+    tpath_mongo = item['testpath'].replace('/','\\')
+
+    if init == 'U' or init == 'V' or init == 'W':
+        ppath = 'D:\\ryosuke-ku\\data_set\\Git_20161108\\UVW\\' + ppath_mongo
+        tpath = 'D:\\ryosuke-ku\\data_set\\Git_20161108\\UVW\\' + tpath_mongo
+        # writer.writerow([project_name, ppath, tpath])
+        print('ppath : ' + ppath + ' , ' + 'tpath : ' + tpath)
+        array.append(project_name + ',' + tpath + ',' + ppath)
+
+    else:
+        num += 1
+    cnt += 1
+print(cnt)
+print(num)
+
+cleanArray = list(set(array))
+print(cleanArray)
+reigai = 0
+for info in cleanArray:
+    infoArray = info.split(',')
+    print(infoArray)
+    print(len(infoArray))
+    if len(infoArray) != 3:
+        reigai += 1
+    else:
+        writer.writerow(infoArray)
+
+
+print('reigai : ' + str(reigai))
+print('detect projects' + str(len(cleanArray)))
---
+++ generateCSV/generate_csv_XYZ.py
@@ -0,0 +1,50 @@
+from pymongo import MongoClient
+import csv
+
+
+clint = MongoClient()
+db = clint['testMapList']
+
+# items = db.mapingCollection.find({'startline1':11,'endline1':16})
+items = db.mapingCollection.find()
+cnt = 0
+num = 0
+file = open('mongoInfo_XYZ.csv','w')
+writer = csv.writer(file)
+
+array = []
+for item in items:
+    project_name = item['path'][:item['path'].find('/')]
+    # print(project_name)
+    init = item['path'][:1].upper()
+    ppath_mongo = item['path'].replace('/','\\')
+    tpath_mongo = item['testpath'].replace('/','\\')
+
+    if init == 'X' or init == 'Y' or init == 'Z':
+        ppath = 'D:\\ryosuke-ku\\data_set\\Git_20161108\\XYZ\\' + ppath_mongo
+        tpath = 'D:\\ryosuke-ku\\data_set\\Git_20161108\\XYZ\\' + tpath_mongo
+        # writer.writerow([project_name, ppath, tpath])
+        print('ppath : ' + ppath + ' , ' + 'tpath : ' + tpath)
+        array.append(project_name + ',' + tpath + ',' + ppath)
+
+    else:
+        num += 1
+    cnt += 1
+print(cnt)
+print(num)
+
+cleanArray = list(set(array))
+print(cleanArray)
+reigai = 0
+for info in cleanArray:
+    infoArray = info.split(',')
+    print(infoArray)
+    print(len(infoArray))
+    if len(infoArray) != 3:
+        reigai += 1
+    else:
+        writer.writerow(infoArray)
+
+
+print('reigai : ' + str(reigai))
+print('detect projects' + str(len(cleanArray)))
